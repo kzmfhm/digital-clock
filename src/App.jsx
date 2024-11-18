580870a41e212059dd08d2a532b0cec2cf2fd65e
kzm/fhm <khuzaimanazik4@gmail.com>
2024-11-18 06:42:47 -0800

    add digital clock

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+import DigitalClock from './DigitalClock.jsx'
+
+function App() {
+
+  return (
+    <>
+   
+    <DigitalClock/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/DigitalClock.jsx
@@ -0,0 +1,42 @@
+import React ,{useEffect,useState} from 'react'
+
+const DigitalClock = () => {
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date() )
+        }, 1000)
+        return (() => {
+            clearInterval(intervalId)
+        })
+    }, [])
+
+    function formattedTime(){
+        let hours = time.getHours()
+        const minutes = time.getMinutes()
+        const seconds = time.getSeconds()
+        const meridiem = hours >= 12 ? "PM" : "AM"
+
+        hours = hours % 12 || 12
+
+        return `${padZero(hours)}: ${padZero(minutes)} :${padZero(seconds)} ${meridiem}`
+    }
+
+    function padZero(number){
+        return (number < 10 ? '0' : "") + number
+    }
+
+  return (
+    <> 
+    <div className='clock-container'>
+    <h1>Digital Clock</h1>
+        <div className="clock">
+            <span>{formattedTime()}</span>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default DigitalClock
