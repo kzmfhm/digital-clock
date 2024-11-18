dcbad6416e7c34bde8643442fe5c1ee37e3d62af
kzm/fhm <khuzaimanazik4@gmail.com>
2024-11-18 06:58:26 -0800

    refactor styling

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -4,13 +4,15 @@ const DigitalClock = () => {
     const [time, setTime] = useState(new Date())
 
     useEffect(() => {
+
         const intervalId = setInterval(() => {
-            setTime(new Date() )
+            setTime(new Date())
         }, 1000)
         return (() => {
             clearInterval(intervalId)
         })
-    }, [])
+
+    },[])
 
     function formattedTime(){
         let hours = time.getHours()
